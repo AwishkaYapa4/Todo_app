1e95b7f38a03a79ae3b529e336435e7ed9643e0b
Awishka Yapa <168206870+AwishkaYapa4@users.noreply.github.com>
2025-08-29 12:46:38 +0530

    done the project

--- lib/auth/main_page.dart
+++ lib/auth/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/const/color.dart';
 import 'package:todoapp/screen/home.dart';
 import 'package:todoapp/auth/auth_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -9,16 +10,6 @@ class Main_Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-            },
-          ),
-        ],
-      ),
       body: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -6,6 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:todoapp/data/fairstore.dart';
 import 'package:todoapp/widget/stream_note.dart';
 import 'package:todoapp/widget/task_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Home_Screen extends StatefulWidget {
   const Home_Screen({super.key});
@@ -21,13 +22,44 @@ class _Home_ScreenState extends State<Home_Screen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColors,
+      appBar: AppBar(
+        backgroundColor: backgroundColors,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: GestureDetector(
+              onTap: () async {
+                await FirebaseAuth.instance.signOut();
+              },
+              child: Container(
+                width: 80,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: custom_green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    'Log Out',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: Visibility(
         visible: show,
         child: FloatingActionButton(
           onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => Add_creen(),
-            ));
+            Navigator.of(
+              context,
+            ).push(MaterialPageRoute(builder: (context) => Add_creen()));
           },
           backgroundColor: custom_green,
           child: Icon(Icons.add, size: 30),
@@ -54,9 +86,10 @@ class _Home_ScreenState extends State<Home_Screen> {
               Text(
                 'isDone',
                 style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey.shade500,
-                    fontWeight: FontWeight.bold),
+                  fontSize: 16,
+                  color: Colors.grey.shade500,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               Stream_note(true),
             ],
